a482984c896e4f73ab92cd25431f262e3d8021de
Mohankrishna1201 <21EE01037@iitbbs.ac.in>
2024-03-08 11:45:01 +0530

    added 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignOut" element={<SignOut />} />
+        <Route path="/About" element={<About />} />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
